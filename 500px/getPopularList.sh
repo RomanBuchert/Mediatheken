ebdca9eed041c85b65c98340dc8983f6ce9a651b
rb <rb@elektronik.local>
2013-05-22 16:29:28 +0200

    Ausgabe von wget durch Infotext ersetzt.

--- 500px/getPopularList.sh
+++ 500px/getPopularList.sh
@@ -4,6 +4,7 @@
 KATEGORIE=popular
 
 for (( SEITE = 1 ; SEITE < 5000 ; SEITE++ )) ; do
+  echo getting Page $SEITE...
   wget http://www.500px.com/photos?page=$SEITE -O ./pages/$KATEGORIE$SEITE.html >/dev/null 2>&1
   grep "next_page disabled" ./pages/$KATEGORIE$SEITE.html
   ERGEBNIS=$?
