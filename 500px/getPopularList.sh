3dc3c7750102a7a6357a1d7f93e621900b5a1d98
rb <rb@elektronik.local>
2013-05-22 11:21:57 +0200

    Es werden nur noch vorhandene Übersichtsseiten heruntergeladen.

--- 500px/getPopularList.sh
+++ 500px/getPopularList.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Diese Datei läd alle Links zu Fotos auf der Seite 500px.com herunter
 
-for (( SEITE = 1 ; SEITE < 1000 ; SEITE++ )) ; do
-  wget http://www.500px.com/photos?page=$SEITE -O ./pages/page$SEITE.html
+for (( SEITE = 1 ; SEITE < 5000 ; SEITE++ )) ; do
+  wget http://www.500px.com/photos?page=$SEITE -O ./pages/popular$SEITE.html >/dev/null 2>&1
+  grep "next_page disabled" ./pages/popular$SEITE.html
+  ERGEBNIS=$?
+  if [ $ERGEBNIS != 1 ] ; then
+    exit 1
+  fi
 done
 
