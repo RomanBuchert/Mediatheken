52d18875f5cd76b8d1aeb071e89a25f0c6f257e1
rb <rb@elektronik.local>
2013-05-22 12:55:30 +0200

    Kategorien eingefügt

--- 500px/getPopularList.sh
+++ 500px/getPopularList.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Diese Datei läd alle Links zu Fotos auf der Seite 500px.com herunter
 
+KATEGORIE=popular
+
 for (( SEITE = 1 ; SEITE < 5000 ; SEITE++ )) ; do
-  wget http://www.500px.com/photos?page=$SEITE -O ./pages/popular$SEITE.html >/dev/null 2>&1
-  grep "next_page disabled" ./pages/popular$SEITE.html
+  wget http://www.500px.com/photos?page=$SEITE -O ./pages/$KATEGORIE$SEITE.html >/dev/null 2>&1
+  grep "next_page disabled" ./pages/$KATEGORIE$SEITE.html
   ERGEBNIS=$?
   if [ $ERGEBNIS != 1 ] ; then
     exit 1
