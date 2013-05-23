9e8dde77ff1b7395adfcfc8386e6809610506db4
rb <rb@elektronik.local>
2013-05-23 09:23:36 +0200

    diverse Verbesserungen

--- 500px/genPhotoLinks.sh
+++ 500px/genPhotoLinks.sh
@@ -2,9 +2,9 @@
 # Diese Datei erstellt eine Datei deren Inhalt die Adresse zu den Fotos auf 500px.com ist.
 
 rm Fotos.tmp
-rm Fotos.txt
+rm Fotos.tmp2
 for DATEI in `ls pages/*.html` ; do
   grep 3.jpg $DATEI | awk {'print $3'} | sed s/src=// >> Fotos.tmp
 done
-sort -t\/ -n -k 3 Fotos.tmp | sed s/3.jpg/5.jpg/ > Fotos.txt
-rm Fotos.tmp
+cat Fotos.tmp Fotos.txt | sed s/3.jpg/5.jpg/ >> Fotos.tmp2
+cat Fotos.tmp2 | sort | uniq > Fotos.txt
